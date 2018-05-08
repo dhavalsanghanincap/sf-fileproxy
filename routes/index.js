d62cbf406bab87a6ba4d7299727558cf43c8badb
Dhaval Sanghani <31955087+dhavalsanghanincap@users.noreply.github.com>
2018-05-08 16:16:07 +0530

    Update index.js

--- routes/index.js
+++ routes/index.js
@@ -63,7 +63,7 @@ const retrieveToken = () => {
     console.log('----> Obtaining OAuth Token...');
     request.post(options, (err, resp, body) => {
       const resultJson = JSON.parse(body);
-      reject({message:JSON.stringify(oauth});
+      reject({message:JSON.stringify(config.oauth});
       if (!err) {
         console.log('----> Token Obtained. Verifying that API Key present and valid with server...');
         resolve(resultJson);
